a48757a1078c13ee6c4b9101a1f6e94da09ae101
fabioune <fabien.bauculat@gmail.com4>
2019-02-19 09:36:45 +0100

    Ajout de resultprono dans AbstractUser Ajout de l'id et des boolean team1 team2 dans AbstractPronostic

--- src/main/java/fr/ipme/coupe/core/AbstractPronostic.java
+++ src/main/java/fr/ipme/coupe/core/AbstractPronostic.java
@@ -1,4 +1,39 @@
 package fr.ipme.coupe.core;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 abstract class AbstractPronostic {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private long id;
+    private boolean team1;
+    private boolean team2;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public boolean isTeam1() {
+        return team1;
+    }
+
+    public void setTeam1(boolean team1) {
+        this.team1 = team1;
+    }
+
+    public boolean isTeam2() {
+        return team2;
+    }
+
+    public void setTeam2(boolean team2) {
+        this.team2 = team2;
+    }
 }
